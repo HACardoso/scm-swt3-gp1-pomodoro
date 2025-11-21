e1f6c499421bc582d9f257e4543df2533d47a12b
Hugo Cardoso <hac@cin.ufpe.br>
2025-11-20 22:35:59 -0300

    Adicionando arquivos e passtas para organizacao do projeto. Adicao de arquvo Dockerfile para execucao em container da aplicacao

---
+++ app/app.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+def myapp():
+    # Criacao da janela
+    root = tk.Tk()
+    root.title("Mileage tracker")
+    root.geometry("400x300")
+
+
+    # Inicia o loop da aplicação
+    root.mainloop()
+
+
+
+if __name__ == '__main__':
+    myapp()

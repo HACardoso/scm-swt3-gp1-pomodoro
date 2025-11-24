c9790909adca58d80a635701ce61aaf3b23c9522
Hugo Cardoso <hac@cin.ufpe.br>
2025-11-24 11:53:05 -0300

    Modificando a estrutura do codigo do aplicativo para que seja utilizado o padrao POO, facilitando o desenvolvimento e teste do aplicativo

--- app/app.py
+++ app/app.py
@@ -1,16 +1,17 @@
 import tkinter as tk
 
-def myapp():
-    # Criacao da janela
-    root = tk.Tk()
-    root.title("Mileage tracker")
-    root.geometry("400x300")
-
-
-    # Inicia o loop da aplicação
-    root.mainloop()
+class MileageTracker:
+    def __init__(self, root):
+        # Criacao da janela
+        self.root = root
+        root.title("Mileage tracker")
+        root.geometry("400x300")
+        # Inicia o loop da aplicação
+        
 
 
 
 if __name__ == '__main__':
-    myapp()
+    root = tk.Tk()
+    app = MileageTracker(root)
+    root.mainloop()
